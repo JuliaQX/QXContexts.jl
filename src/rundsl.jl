462fa0813c1635382f9c0ca71a15875befbe02a3
Kenneth Hanley <kenneth.hanley@ichec.ie>
2021-02-04 09:39:51 +0000

    Add simple command line script

---
+++ src/rundsl.jl
@@ -0,0 +1,57 @@
+using QXRun
+using ArgParse
+
+"""
+    parse_commandline(ARGS)
+
+Parse command line arguments and return argument dictionary
+"""
+function parse_commandline(ARGS)
+    s = ArgParseSettings("QXRun")
+
+    @add_arg_table! s begin
+        "--dsl", "-d"
+            help = "DSL file path"
+            required = true
+            arg_type = String
+        "--parameter-file", "-p"
+            help = "Parameter file path"
+            default = nothing
+            arg_type = Union{Nothing, String}
+        "--input", "-i"
+            help = "Input data file path"
+            required = true
+            arg_type = String
+        "--output", "-o"
+            help = "Output data file path (defaults to input file)"
+            default = nothing
+            arg_type = Union{Nothing, String}
+        "-v"
+            help = "Enable verbose output"
+            action = :count_invocations
+    end
+
+    return parse_args(ARGS, s)
+end
+
+"""
+    main(ARGS)
+
+QXRun entry point
+"""
+function main(ARGS)
+    args = parse_commandline(ARGS)
+
+    dsl_file       = args["dsl"]
+    parameter_file = args["parameter-file"]
+    input_file     = args["input"]
+    output_file    = args["output"] === nothing ? input_file : args["output"]
+    verbose        = args["v"]
+
+    results = execute(dsl_file, parameter_file, input_file, output_file)
+end
+
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    main(ARGS)
+end
