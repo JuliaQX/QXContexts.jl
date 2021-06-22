6ad01c4caeafc5c33517961218993b09cb4d22d4
John Brennan <john.brennan@ichec.ie>
2021-06-22 10:21:59 +0100

    Add timing scripts for larger sizes

---
+++ hack/initial_profiles/6_timings.jl
@@ -0,0 +1,46 @@
+using BenchmarkTools
+using CUDA
+using Statistics
+
+using QXContexts
+
+function main(args)
+    file_path  = @__DIR__
+    dsl_file   = joinpath(dirname(dirname(file_path)), "examples/rqc/rqc_7_7_32.qx")
+    input_file = joinpath(dirname(dirname(file_path)), "examples/rqc/rqc_7_7_32.jld2")
+
+    cg, _ = parse_dsl_files(dsl_file, input_file)
+
+    # get time on cpu
+    ctx_cpu = QXContext{Array{ComplexF32}}(cg)
+    set_open_bonds!(ctx_cpu)
+    t = @elapsed ctx_cpu() # run to ensure all is precompiled
+    @info "CPU warmup ran in $t"
+    b = @benchmark $(ctx_cpu)() # benchmark run
+    @info "CPU benchmark time"
+    @show b
+
+    # get time on gpu
+    ctx_gpu = QXContext{CuArray{ComplexF32}}(cg)
+    set_open_bonds!(ctx_gpu)
+    t = @elapsed CUDA.@sync begin ctx_gpu() end # run to ensure all is precompiled
+    @info "GPU warmup ran in $t"
+    #b = @benchmark CUDA.@sync begin $(ctx_gpu)() end # benchmark run
+    CUDA.@time ctx_gpu()
+    #@info "GPU benchmark time"
+    #@show median(ts)
+    ts = []
+    for i in 1:5
+        t = CUDA.@elapsed ctx_gpu()
+        @info "GPU run $i in $t"
+        flush(stdout)
+        push!(ts, t)
+        sleep(1)
+    end
+    @info "GPU times: $(ts)"
+    @info "Mean time: $(mean(ts))"
+    flush(stdout)
+    nothing
+end
+
+main(ARGS)
---
+++ hack/initial_profiles/6_timings_submit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH -A tra21_hackathon
+#SBATCH --time 1:00:00     # format: HH:MM:SS
+#SBATCH -N 1                # 1 node
+#SBATCH --ntasks-per-node=8 # 8 tasks out of 128
+#SBATCH --gres=gpu:4        # 1 gpus per node out of 4
+#SBATCH --exclusive
+#SBATCH --mem=16000          # memory per node out of 246000MB
+#SBATCH --job-name=TestProfile
+#SBATCH --reservation=s_tra_hackathon21
+
+module load profile/advanced
+module load autoload julia/1.7--gnu--8.4.0
+
+julia --project=../../ 6_timings.jl
